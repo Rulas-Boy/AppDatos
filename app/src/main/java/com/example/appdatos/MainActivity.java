b61dba3288648fd369f2eeec01333a7b1db0327a
rulas <raul.alvarez18@tectijuana.edu.mx>
2022-05-19 17:18:57 -0700

    Agrego actividad de ConsultaUsuario

--- app/src/main/java/com/example/appdatos/MainActivity.java
+++ app/src/main/java/com/example/appdatos/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appdatos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        ConexionSQLiteHelper conexion = new ConexionSQLiteHelper(this, "bd_usuarios", null, 1);
+    public void RegistroUsuario(View view) {
+        Intent registar = new Intent(this, RegistroUsuarios.class);
+        startActivity(registar);
+    }
+
+    public void ConsultaUsuario(View view) {
+        Intent consulta = new Intent(this, ConsultaUsuario.class);
+        startActivity(consulta);
+    }
+
+    public void ConsultaSpinner(View view) {
+        Intent consulta = new Intent(this, RegistroUsuarios.class);
+        startActivity(consulta);
+    }
+
+    public void ConsultaLista(View view) {
+        Intent consulta = new Intent(this, RegistroUsuarios.class);
+        startActivity(consulta);
     }
 }
